e0d4953aa3bbc1c0cff2c3664a78119479c8a6f1
Andrea Gonzalez A <a.gzzart@gmail.com>
2018-10-18 12:06:35 -0500

    fixing images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Hero = () => (
   <div className="hero">
 		<Link to={'/'} >
-			<img className="logo" src="/images/logo.png" alt="logo armyofmakers"/>
+			<img className="logo" src="./images/logo.png" alt="logo armyofmakers"/>
 		</Link>
     <i className="fas fa-chevron-down"></i>
   </div>
